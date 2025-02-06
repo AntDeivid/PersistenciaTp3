f440be2910297e26dde3147fb1ab09f35721f160
Jeferson Aires <jefersonaires.jittechnology@gmail.com>
2025-02-05 22:08:08 -0300

    correção na classe de Pagamento

--- src/app/models/pagamento.py
+++ src/app/models/pagamento.py
@@ -13,3 +13,6 @@ class Pagamento(BaseModel):
 
     class Config:
         allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {datetime: lambda v: v.isoformat()}
+        
