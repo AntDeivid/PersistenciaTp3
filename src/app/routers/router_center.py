57aa90ea076c5322f81c17f5f4b998e2ea58335a
Jeferson Aires <jefersonaires.jittechnology@gmail.com>
2025-02-05 23:14:56 -0300

    adição ao router center

--- src/app/routers/router_center.py
+++ src/app/routers/router_center.py
@@ -2,8 +2,10 @@ from fastapi.routing import APIRouter
 
 from src.app.routers.usuario_router import usuario_router
 from src.app.routers.pagamento_router import pagamento_router
+from src.app.routers.manutencao_router import manutencao_router
 
 router = APIRouter()
 
 router.include_router(usuario_router)
 router.include_router(pagamento_router)
+router.include_router(manutencao_router)
