296b710144880bff6f359092cf6338c771461bd1
Jeferson Aires <jefersonaires.jittechnology@gmail.com>
2025-02-20 00:07:17 -0300

    correção no dto de manutencao

--- src/app/dtos/manutencao_dto.py
+++ src/app/dtos/manutencao_dto.py
@@ -1,25 +1,21 @@
-from typing import Optional
 from datetime import datetime
-
+from typing import Optional
 from pydantic import BaseModel
-
 from src.app.models.manutencao import Manutencao
-
 
 class ManutencaoDTO(BaseModel):
     id: Optional[str] = None
-    data_manutencao: datetime
-    descricao: str
-    custo : float
+    data: datetime
+    tipo_manutencao: str
+    custo: float
     observacao: str
-    
 
     @classmethod
     def from_model(cls, manutencao: Manutencao):
         return cls(
-            id=str(manutencao.id),
-            data_manutencao=manutencao.data_manutencao,
-            descricao=manutencao.descricao,
+            id=str(manutencao.id) if manutencao.id else None,
+            data=manutencao.data,
+            tipo_manutencao=manutencao.tipo_manutencao,
             custo=manutencao.custo,
             observacao=manutencao.observacao
         )
@@ -27,10 +23,8 @@ class ManutencaoDTO(BaseModel):
     def to_model(self) -> Manutencao:
         return Manutencao(
             id=self.id,
-            data_manutencao=self.data_manutencao,
-            descricao=self.descricao,
+            data=self.data,
+            tipo_manutencao=self.tipo_manutencao,
             custo=self.custo,
             observacao=self.observacao
         )
-        
-        
